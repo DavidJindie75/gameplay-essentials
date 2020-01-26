b0d84b76bf311709a23b161154c29974c1f60e04
spartan6nm <59691958+spartan6nm@users.noreply.github.com>
2020-01-26 14:12:52 +0330

    spartan movement testing

---
+++ Assets/Spartan/Scripts/ClickBasedMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickBasedMove : MonoBehaviour
+{
+
+    [SerializeField] 
+    private LayerMask moveableground;
+
+    [SerializeField]
+    private Camera cam;
+
+    private NavMeshAgent pathFinderAgent;
+
+    Ray theRay;
+
+    RaycastHit rayCastInfo;
+
+
+    void Awake()
+    {
+        pathFinderAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("you clicked");
+
+            theRay = cam.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(theRay, out rayCastInfo))
+            {
+                Debug.Log("ray was casted");
+
+                pathFinderAgent.SetDestination(rayCastInfo.point);
+
+                Debug.Log(pathFinderAgent.steeringTarget);
+            }
+        }
+    }
+}
