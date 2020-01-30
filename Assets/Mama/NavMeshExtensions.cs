2b86c37a855944f79e26db8036723dd2791b200a
joshlynnv <codecomputing00@gmail.com>
2020-01-29 22:44:09 -0500

    Add files via upload

---
+++ Assets/Mama/NavMeshExtensions.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class NavMeshExtensions
+{
+    public static void ResumeAgent(NavMeshAgent nva)
+    {
+        nva.isStopped = false;
+    }
+
+    public static void StopAgent(NavMeshAgent nva)
+    {
+        nva.isStopped = true;
+    }
+
+    public static void GoToSetPosition(Vector3 tp, NavMeshAgent nva)
+    {
+        ResumeAgent(nva);
+        nva.SetDestination(tp);
+    }
+
+    public static void StopMoving(NavMeshAgent nva)
+    {
+        nva.ResetPath();
+
+    }
+}
---
+++ Assets/Mama/StaticTags.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StaticTags
+{
+    public static string Respawn = "Respawn";
+    public static string Mount = "Mount";
+    public static string Enemy = "Enemy";
+    public static string Joystick = "Joystick";
+    public static string Ally = "Ally";
+    public static string Water = "Water";
+    public static string Wall = "Wall";
+    public static string Minimap = "Minimap";
+    public static string Collide = "Collide";
+    public static string SpawnPoint = "SpawnPoint";
+    public static string Terrain = "Terrain";
+}
